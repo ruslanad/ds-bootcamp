53f8d73ae1e5122299f027a113a4da0372a079b3
Ruslana Dalinina <ruslanad@LMJENNIFERF05104.localdomain>
2019-05-31 13:50:55 -0700

    session 3

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import yaml
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def create_app():
     app = Flask(__name__)
+    with open(os.path.join(dir_path, 'yamls/ruslana_model.yaml'), 'r') as f:
+        model_config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    @app.route("/")
+    @app.route("/ruslana_model/<submodel>/<x_1>")
+    def ruslana_model(submodel, x_1):
+        try:
+            submodel = model_config[submodel]
+        except(KeyError):
+            return 'Model not found', 404
+        dict_ = {'score': submodel['a_1']+submodel['b_1'] * int(x_1)}
+        return jsonify(dict_)
+
+    @app.route("/ruslana")
     def hello_ruslana():
         return "Hi Ruslana!", 200
 
-    @app.route("/bye")
+    @app.route("/props")
     def props():
         return "You are great!", 200
 
--- tests/test_ruslana.py
+++ tests/test_ruslana.py
@@ -1,5 +1,21 @@
 
 
-def test_get_metadata(client):
-    res = client.get("/")
+def test_get_metadata_ruslana(client):
+    res = client.get("/ruslana")
     assert res.status_code == 200
+
+
+def test_get_metadata_props(client):
+    res = client.get("/props")
+    assert res.status_code == 200
+
+
+def test_get_ruslana_model(client):
+    res = client.get("/ruslana_model/submodel_1/10")
+    assert res.status_code == 200
+    assert res.json['score'] == 10
+
+
+def test_invalid_model(client):
+    res = client.get("/ruslana_model/submodel_3/10")
+    assert res.status_code == 404
