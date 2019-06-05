fe2b858b6e52bb83c7a9c584bed9e828a885da4f
Ruslana Dalinina <ruslanad@LMJENNIFERF05104.localdomain>
2019-06-05 11:46:03 -0700

    l4

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -4,21 +4,14 @@ import yaml
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+MAX_VALUE = 10000
+MIN_VALUE = 0
 
 
 def create_app():
     app = Flask(__name__)
     with open(os.path.join(dir_path, 'yamls', 'ruslana_model.yaml'), 'r') as f:
         model_config = yaml.load(f, Loader=yaml.SafeLoader)
-
-    @app.route("/ruslana_model/<submodel>/<x_1>")
-    def ruslana_model(submodel, x_1):
-        try:
-            submodel = model_config[submodel]
-        except(KeyError):
-            return 'Model not found', 404
-        dict_ = {'score': submodel['a_1']+submodel['b_1'] * int(x_1)}
-        return jsonify(dict_)
 
     @app.route("/ruslana")
     def hello_ruslana():
@@ -27,6 +20,21 @@ def create_app():
     @app.route("/props")
     def props():
         return "You are great!", 200
+
+    @app.route("/ruslana_model/<submodel>/<x_1>")
+    def ruslana_yaml_model(submodel, x_1):
+        x_1 = int(x_1)
+
+        if x_1 > MAX_VALUE:
+            return f"Value {x_1} > {MAX_VALUE}", 400
+        elif x_1 < MIN_VALUE:
+            return f"Value {x_1} < {MIN_VALUE}", 400
+        try:
+            submodel = model_config[submodel]
+        except(KeyError):
+            return 'Model not found', 404
+        dict_ = {'score': submodel['a_1']+submodel['b_1'] * x_1}
+        return jsonify(dict_)
 
     return app
 # FLASK_APP=bootcamp_ds/flask_app.py flask run
--- tests/test_ruslana.py
+++ tests/test_ruslana.py
@@ -1,4 +1,7 @@
 
+from hypothesis import strategies as st, given, reproduce_failure
+from ds_bootcamp.flask_app import MIN_VALUE, MAX_VALUE
+
 
 def test_get_metadata_ruslana(client):
     res = client.get("/ruslana")
@@ -13,9 +16,39 @@ def test_get_metadata_props(client):
 def test_get_ruslana_model(client):
     res = client.get("/ruslana_model/submodel_1/10")
     assert res.status_code == 200
-    assert res.json['score'] == 10
+    assert res.json['score'] == 11
 
 
 def test_invalid_model(client):
+    res = client.get("/ruslana_model/submodel_3/10")
+    assert res.status_code == 404
+
+
+@given(
+    x=st.integers(min_value=MIN_VALUE, max_value=MAX_VALUE)
+)
+def test_ruslana_model_hypothesis(client, x):
+    res = client.get(f"/ruslana_model/submodel_1/{x}")
+    assert res.status_code == 200
+    assert res.json['score'] > x
+
+
+@given(
+    x=st.integers(min_value=MAX_VALUE+1)
+)
+def test_ruslana_model_hypothesis_low_value(client, x):
+    res = client.get(f"/ruslana_model/submodel_1/{x}")
+    assert res.status_code == 400
+
+
+@given(
+    x=st.integers(max_value=MIN_VALUE-1)
+)
+def test_ruslana_model_hypothesis_low_value(client, x):
+    res = client.get(f"/ruslana_model/submodel_1/{x}")
+    assert res.status_code == 400
+
+
+def test_ruslana_model_404(client):
     res = client.get("/ruslana_model/submodel_3/10")
     assert res.status_code == 404
